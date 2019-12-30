626ca1870b0cf95eeb970ef2c7cc6d6d44acaa40
Qicz <qiczzhu@gmail.com>
2019-12-30 23:25:27 +0800

    refactor

--- learning-springboot2-web/src/main/java/cn/zhucongqi/LearningSpringBoot2Application.java
+++ learning-springboot2-web/src/main/java/cn/zhucongqi/LearningSpringBoot2Application.java
@@ -2,9 +2,11 @@ package cn.zhucongqi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@EnableTransactionManagement
 @MapperScan(basePackages = "cn.zhucongqi.mapper")
 public class LearningSpringBoot2Application {
 
